8a624d1518c06139002d086008aaf728e6941dc6
Sheharyar <sheharyarone@gmail.com>
2022-10-06 02:26:51 +0500

    Double scoring bug

--- 9.Yahtzee/src/RuleRow.js
+++ 9.Yahtzee/src/RuleRow.js
@@ -3,8 +3,9 @@ import './RuleRow.css'
 
 class RuleRow extends Component {
   render() {
+    const notDisabled = this.props.score===undefined; // TRUE WHEN NOT USED ONCE
     return (
-      <tr className="RuleRow RuleRow-active" onClick={this.props.doScore}>
+      <tr className={`RuleRow RuleRow-${ notDisabled ? 'active' : 'disabled'}`} onClick={notDisabled ? this.props.doScore : null}>
         <td className="RuleRow-name">{this.props.name}</td>
         <td className="RuleRow-score">{this.props.score}</td>
       </tr>
