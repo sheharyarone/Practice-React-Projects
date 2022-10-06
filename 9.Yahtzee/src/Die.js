f0b531c2d7f25653380aed27be36abd33022dca4
Sheharyar <sheharyarone@gmail.com>
2022-10-06 11:14:54 +0500

    animation

--- 9.Yahtzee/src/Die.js
+++ 9.Yahtzee/src/Die.js
@@ -3,13 +3,15 @@ import "./Die.css";
 
 class Die extends Component {
   static defaultProps = {
-    icons : ['one','two','three','four','five','six']
+    icons : ['one','two','three','four','five','six'],
+    val:6
   }
   render() {
-    const {icons,locked,val,handleClick}=this.props;
+    const {icons,locked,val,handleClick,rolling}=this.props;
     let classes=`Die fas fa-dice-${icons[val-1]} fa-5x `;
     
     if (locked) classes+='Die-locked';
+    if(rolling && !locked) classes+='Die-rolling';
     return (
       <i
         className={classes}
