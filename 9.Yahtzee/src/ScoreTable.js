c592c3aa269747c683f5438b15b1e65991e3fb91
Sheharyar <sheharyarone@gmail.com>
2022-10-07 00:58:20 +0500

    totalScore func

--- 9.Yahtzee/src/ScoreTable.js
+++ 9.Yahtzee/src/ScoreTable.js
@@ -6,9 +6,18 @@ import { ones, twos, threes, fours, fives, sixes, threeOfKind, fourOfKind, fullH
 
 class ScoreTable extends Component {
 
+  getTotalScore(){
+    const {scores}=this.props;
+    let totalScore=0;
+    for(let key in scores){
+      if (scores[key]) totalScore+=scores[key];
+    }
+    return totalScore;
+  }
+
   render() {
     const { scores, doScore } = this.props;
-
+    
     return (
       <div className="ScoreTable">
         <section className="ScoreTable-section">
@@ -38,6 +47,7 @@ class ScoreTable extends Component {
             </tbody>
           </table>
         </section>
+        <h2>TOTAL SCORE : {this.getTotalScore()}</h2>
       </div>
     )
   }
