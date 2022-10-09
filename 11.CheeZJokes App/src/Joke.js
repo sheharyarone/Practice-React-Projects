c626a90bead0186ce5123b73b8049e52575858b6
Sheharyar <sheharyarone@gmail.com>
2022-10-09 17:44:34 +0500

    child component Joke

---
+++ 11.CheeZJokes App/src/Joke.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Joke extends Component {
+    render(){
+        return(
+            <div>
+                <li>
+                    {this.props.joke}
+                </li>
+            </div>
+        )
+    }
+
+}
+export default Joke;
--- 11.CheeZJokes App/src/JokesList.js
+++ 11.CheeZJokes App/src/JokesList.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios';
-
+import Joke from './Joke';
 const API_URL = 'https://icanhazdadjoke.com/slack';
 
 class JokesList extends Component {
@@ -12,8 +12,9 @@ class JokesList extends Component {
     }
     async componentDidMount() {
 
-        for (let i = 0; i < 10; i++) {
+        for (let i = 0; i < 5; i++) {
             let response = await axios.get(API_URL);
+            // console.log(response.data);
             let joke = response.data.attachments[0].text;
             this.setState(st => ({
                 List: [
@@ -21,25 +22,18 @@ class JokesList extends Component {
                     joke
                 ]
             })
-
             )
-            console.log(joke);
-
         }
     }
 
     render() {
-        let ren = this.state.List.map(J => (
-            
-                <li>
-                    {J}
-                </li>
-            
+        let Jokes = this.state.List.map(J => (
+            <Joke joke={J} />
         ))
         return (
             <div>
                 <h1>CHEEZJOKES APP</h1>
-                <h3>{ren}</h3>
+                <h3>{Jokes}</h3>
             </div>
         )
     }
