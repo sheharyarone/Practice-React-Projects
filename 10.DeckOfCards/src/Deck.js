f91fe45ff79545b4c5483a5650fe24663e831396
Sheharyar <sheharyarone@gmail.com>
2022-10-09 15:00:44 +0500

    Fetch Data in Deck Component

--- 10.DeckOfCards/src/Deck.js
+++ 10.DeckOfCards/src/Deck.js
@@ -1,24 +1,26 @@
 import React, { Component } from 'react';
-import axios from 'axios'
+import axios from 'axios';
 import Card from './Card';
+
+
+const API_URL = 'https://www.deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1';
 
 class DeckOfCard extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            cards: []
+            deck:null
         }
     }
-    async componentDidCount() {
-        const url='https://deckofcardsapi.com/api/deck/new/shuffle/';
-        let response = axios.get(url);
-        let id=response.deck_id;
-        const src=''
-    }   
+
+    async componentDidMount(){
+        let deck=await axios.get(API_URL);
+        this.setState({deck: deck.data});
+    }
     render() {
         return (
             <div>
-
+                
             </div>
         )
     }
