221941ab50d6e406d3c07a3185ad7a5a73aba4cd
Sheharyar <sheharyarone@gmail.com>
2022-10-18 21:30:01 +0500

    alt addition

--- 12.Vending Machine/src/VendingMachine.js
+++ 12.Vending Machine/src/VendingMachine.js
@@ -4,7 +4,7 @@ class VendingMachine extends Component {
     render() {
         return (
             <div>
-                <img src="https://images.unsplash.com/photo-1618506557292-ec1862b3c506?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8dmVuZGluZyUyMG1hY2hpbmV8ZW58MHx8MHx8&w=1000&q=80"  />
+                <img src="https://images.unsplash.com/photo-1618506557292-ec1862b3c506?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8dmVuZGluZyUyMG1hY2hpbmV8ZW58MHx8MHx8&w=1000&q=80" alt='VENDING MACHINE'  />
             </div>
         )
     }
