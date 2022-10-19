41b46ea7bfb640bdfb09c6f0f81cf2b7802cf504
Sheharyar <sheharyarone@gmail.com>
2022-10-19 16:27:57 +0500

    404 page not found

--- 13.FoodPics/src/App.js
+++ 13.FoodPics/src/App.js
@@ -1,12 +1,19 @@
 import Food from './Food';
 import './App.css';
-import { Route,Switch } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
       <Switch>
-        <Route exact path='/:name'  render={(routeProps)=><Food {...routeProps}/>} />
+        <Route exact path='/' render={() => <h1>
+          HOME PAGE
+        </h1>} />
+        <Route exact path='/:name' render={(routeProps) => <Food {...routeProps} />} />
+
+        <Route render={() => <h1>
+          404 PAGE NOT FOUND
+        </h1>} />
       </Switch>
     </div>
   );
