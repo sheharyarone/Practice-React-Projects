eb7c6fd97a800dfe4e66c8aadb37d13d2b6fd256
Sheharyar <sheharyarone@gmail.com>
2022-10-21 22:33:44 +0500

    Navbar styling

--- 13.Dog Info/src/Navbar.js
+++ 13.Dog Info/src/Navbar.js
@@ -1,15 +1,16 @@
 import React, { Component } from 'react';
 import { NavLink } from 'react-router-dom';
 import dogList from './DogList';
+import './Navbar.css';
 class Navbar extends Component {
     render() {
-        
-        const dogsName = dogList.map((dog) => {
-            return <NavLink exact to={`/${dog.name}`} > {dog.name}</NavLink>
 
-        });
+        const dogsName = dogList.map((dog) =>
+            <NavLink exact activeClassName='Navbar-active' to={`/${dog.name}`} > {dog.name}</NavLink>
+        );
         return (
-            <div>
+            <div className='Navbar'>
+                <NavLink exact activeClassName='Navbar-active' to='/'>HOME</NavLink>
                 {dogsName}
             </div>
         )
