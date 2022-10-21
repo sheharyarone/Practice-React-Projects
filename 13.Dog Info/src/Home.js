cb2ef339b617d40de2d2510cb589cb15ed6c8b94
Sheharyar <sheharyarone@gmail.com>
2022-10-22 00:53:08 +0500

    button on img for more info of dog

--- 13.Dog Info/src/Home.js
+++ 13.Dog Info/src/Home.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
+import { Route,Link } from 'react-router-dom';
 import dogList from './DogList';
 import './Home.css';
 class Home extends Component {
     render() {
         const dogsMini = dogList.map((dog) =>
             <div className='item'>
-                <img src={require(`./${dog.src}.jpg`)} alt={dog.name} />
+                <Link to={`/${dog.name}`}>
+                <img src={require(`./${dog.src}.jpg`)} alt={dog.name} onClick={<Route to={`/${dog.name}`} />}/>
+                </Link>
                 <h3>{dog.name}</h3>
             </div>
         );
